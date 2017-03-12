1e71f4e4a239ca835185a10149f1385622210214
marck5 <marck5_2@hotmail.com>
2017-03-11 20:35:37 -0600

    app final

---
+++ ConsultaID.php
@@ -0,0 +1,22 @@
+<?php
+
+include "conexion.php";
+$Id=$_POST["ID"];
+
+$sql="SELECT * FROM servicio where Id_servicio=$Id";
+$result=mysqli_query($conn,$sql);
+$datos= array();
+if($result){
+$i=0;
+while($fila=mysqli_fetch_assoc($result)){
+	$datos[$i]=$fila;
+	$i=$i+1;
+	}
+}else{
+	echo "Error al ejecutar la consulta por ID:".mysqli_error();
+}
+
+mysqli_close($conn);
+echo json_encode($datosId);
+
+?>
---
+++ conexion.php
@@ -0,0 +1,19 @@
+<?php
+/*$servidor="localhost";
+$usuario="root";
+$pwd="alex";
+$BD="servcios_libres";*/
+
+/*$servidor="mysql.hostinger.mx";
+$usuario="u508262852_movil";
+$pwd="sistemas";
+$BD="u508262852_movil";
+
+$conn=mysqli_connect($servidor,$usuario,$pwd,$BD)or die("Error al conectarse a la base de datos".mysqli_error());*/
+	$servidor="mysql.hostinger.mx";
+	$usuario="u160222468_marck";
+	$pws="marck25";
+	$bd="u160222468_marck";
+	
+	$conn=mysqli_connect($servidor,$usuario,$pws,$bd)or die("Error al conectar...".mysqli_error());
+?>
